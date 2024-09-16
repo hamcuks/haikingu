4947558fc2db7dc601aa88bf19cd0df096a881b7
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-09-16 17:07:14 +0700

    chore: setup app project

---
+++ Haikingu Watch App/HaikinguApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaikinguApp.swift
+//  Haikingu Watch App
+//
+//  Created by Ivan Nur Ilham Syah on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Haikingu_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
