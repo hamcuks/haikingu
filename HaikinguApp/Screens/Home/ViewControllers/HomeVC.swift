63da644f6a20d394e6906d3878f67b3d138c0e1e
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-09-30 15:03:02 +0700

    feat!: create home header view

--- HaikinguApp/Screens/Home/ViewControllers/HomeVC.swift
+++ HaikinguApp/Screens/Home/ViewControllers/HomeVC.swift
@@ -6,17 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeVC: UIViewController {
+    
+    let headerView: HomeHeaderView = HomeHeaderView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.configureVC()
+        self.configureHeaderView()
     }
     
     private func configureVC() {
         self.view.backgroundColor = .systemBackground
+    }
+    
+    private func configureHeaderView() {
+        view.addSubview(headerView)
+        
+        headerView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(64)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
     }
 
 }
